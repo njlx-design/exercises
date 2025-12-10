a97ab544a60e9e5ff6c222ac909667b0aeb61321
dell <nj1438096@gmail.com>
2025-12-10 10:20:14 +0300

    v1

--- src/Arrays/Arrayss.java
+++ src/Arrays/Arrayss.java
@@ -42,4 +42,32 @@ public class Arrayss {
         }
         return -1;
     }
+
+    public int binarySearch(int []a, int svalue, int findex, int lindex){
+        while (findex<=lindex){
+            int mid=(findex+lindex)/2;
+            if(svalue==a[mid])
+                return mid;
+            if (svalue>a[mid])
+                findex=mid+1;
+            if (svalue<a[mid])
+                lindex=mid-1;
+        }
+         return -1;
+    }
+
+    public int binarySearch(int []a, int svalue){
+        int findex=0;
+        int lindex=a.length-1;
+        while (findex<=lindex){
+            int mid=(findex+lindex)/2;
+            if(svalue==a[mid])
+                return mid;
+            if (svalue>a[mid])
+                findex=mid+1;
+            if (svalue<a[mid])
+                lindex=mid-1;
+        }
+        return -1;
+    }
 }
--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -1,5 +1,6 @@
 package Arrays;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,6 +10,7 @@ public class Main {
         Arrayss arr = new Arrayss();
 
         int []number={1,2,3,4,5};
+        Arrays.sort(number);
         //System.out.println("Enter" +number.length+" numbers");
         //for (int i=0; i<number.length; i++)
         //  number[i]=scanner.nextInt();
@@ -16,7 +18,8 @@ public class Main {
 
         //arr.update(number , 3 ,9);
         //arr.shiftleft_Delete(number,2);
-        int result= arr.LinearSearch(number , 9);
+        //int result= arr.LinearSearch(number , 9);
+        int result= arr.binarySearch(number , 3);
         if (result!=-1)
             System.out.println("Value found at index :"+result);
         else System.out.println("Value not found");
