4217bd07e8da25b33062b60e9a93553298fa4711
dell <nj1438096@gmail.com>
2025-12-10 11:43:57 +0300

    v1

--- src/SinglyLinkedList/Main.java
+++ src/SinglyLinkedList/Main.java
@@ -1,4 +1,13 @@
 package SinglyLinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list=new SinglyLinkedList<Integer>();
+        list.addLast(30);
+        list.addLast(20);
+        list.addFirst(10);
+        list.addFirst(5);
+        list.removeFirst();
+        list.display();
+    }
 }
