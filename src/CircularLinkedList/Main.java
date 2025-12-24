76d612ac5fa88eb21c78fa592ef0303974e1ce37
dell <nj1438096@gmail.com>
2025-12-24 11:36:02 +0300

    v1

--- src/CircularLinkedList/Main.java
+++ src/CircularLinkedList/Main.java
@@ -4,6 +4,14 @@ import SinglyLinkedList.SinglyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-
+     CircularLinkedList<String> clist=new CircularLinkedList<String>();
+     clist.addFirst("Afnan");
+     clist.addFirst("Nada");
+     clist.addlast("Asmaa");
+     clist.addlast("Sarah");
+     clist.addFirst("Huda");
+     clist.removeLast();
+     clist.removeFirst();
+     clist.display();
     }
 }
